200a9c8bc250a567ce1b6b42c138459b6e2d2011
Amankori2307 <amankori2307@gmail.com>
2019-11-17 17:53:57 +0530

    Updates

---
+++ precache-manifest.2dbd4aa683fd8800eaa4caa609d22c89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b807ef589f40350a6ab9e2804a85a0e5",
+    "url": "/Recipe/index.html"
+  },
+  {
+    "revision": "772487b7556b74feb29b",
+    "url": "/Recipe/static/css/main.646742c2.chunk.css"
+  },
+  {
+    "revision": "6fed3ea56a1630578976",
+    "url": "/Recipe/static/js/2.d56b123d.chunk.js"
+  },
+  {
+    "revision": "772487b7556b74feb29b",
+    "url": "/Recipe/static/js/main.6b2a4422.chunk.js"
+  },
+  {
+    "revision": "26d8401d7db202477bb2",
+    "url": "/Recipe/static/js/runtime-main.e719460e.js"
+  }
+]);
